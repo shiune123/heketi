80937dfebeff67609a562eb2179f803a096c1646
shiune <3228458054@qq.com>
2020-09-17 15:52:10 +0800

    add heketi.log

--- extras/container/heketi-start.sh
+++ extras/container/heketi-start.sh
@@ -197,7 +197,9 @@ if [[ "$(stat -c %s ${HEKETI_PATH}/heketi.db 2>/dev/null)" == 0 && -n "${HEKETI_
     fg %1
 else
     # just start in the foreground
-    bash /usr/bin/recovery.sh &
+    touch /var/log/heketi/recovery.log
+    touch /var/log/heketi/heketi.log
+    bash /usr/bin/recovery.sh >> /var/log/heketi/recovery.log &
     exec "$HEKETI_BIN" --config=/etc/heketi/heketi.json >> /var/log/heketi/heketi.log
 fi
 
